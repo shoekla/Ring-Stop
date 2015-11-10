b2c5bba004661b9f30011fa770aa5a175108d17a
Abir <abirshukla@gmail.com>
2015-11-09 19:05:29 -0500

    Fixed Days Message

--- app/src/main/java/com/example/abirshukla/RingStop/MainActivity.java
+++ app/src/main/java/com/example/abirshukla/RingStop/MainActivity.java
@@ -81,6 +81,9 @@ public class MainActivity extends ActionBarActivity {
                     String mode = addedData.getString("mode");
                     String endTimeS = addedData.getString("endTimeStr");
                     String days = addedData.getString("days");
+                    if (!days.contains("TH")) {
+                        days = days.replace("H","TH");
+                    }
                     if (!list.contains(className)) {
                         listTime.addToList(className, mode, endTimeS, beginTime, endTime, days);
                         list.add(className);
